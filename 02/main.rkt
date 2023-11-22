3a86a2ea91ab9785036ea895ebfd80d4aeaf89bd
jakobottar <jakobottar@pm.me>
2023-11-21 21:33:06 -0700

    remove redundant +

--- 02/main.rkt
+++ 02/main.rkt
@@ -7,14 +7,14 @@
          [(0) 3] ; draw
          [(1) 6] ; win
          [(2) 0]) ; lose
-       (+ mine 1)))) ; add add score for my letter
+       mine 1))) ; add score for my letter
 
 (define (score-p2 line)
   (let* ([theirs (- (char->integer (string-ref line 0)) (char->integer #\A))] ; code of their letter
          [result (- (char->integer (string-ref line 2)) (char->integer #\X))] ; code of the result
          [mine (modulo (+ theirs result -1) 3)]) ; code of my letter (p1's mod table but reverse)
 
-    (+ (* result 3) (+ mine 1)))) ; add add score for my letter
+    (+ (* result 3) mine 1))) ; add score for my letter
 
 (printf "part 1 example: ~a\n" (apply + (map score-p1 (file->lines "example.txt"))))
 (printf "part 1: ~a\n" (apply + (map score-p1 (file->lines "input.txt"))))
